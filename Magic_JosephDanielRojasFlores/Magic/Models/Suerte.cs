9e8c8eb51b3e5f71b97d85e8ac7f7901b6edd99f
Joseph Daniel Rojas Flores <a2019113821@estudiantes.upsa.edu.bo>
2021-10-13 21:14:26 -0400

    Creado el Modelo Suerte con sus atributos: FuturoId, Vision, Imagen

--- Magic_JosephDanielRojasFlores/Magic/Models/Suerte.cs
+++ Magic_JosephDanielRojasFlores/Magic/Models/Suerte.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,14 @@ namespace Magic.Models
 {
     public class Suerte
     {
+        [Key]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Este espacio es requerido")]
+        [StringLength(100, MinimumLength = 50)]
+        public string FuturoId { get; set; }
+        [Required(ErrorMessage = "Espacio requerido")]
+        [StringLength(300, ErrorMessage = "Necesitas llenar este espacio", MinimumLength = 150)]
+        public string Vision { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Necesitas colocar un url de imagen en este espacio")]
+        public string Imagen { get; set; }
     }
 }
